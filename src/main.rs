c4ce4e89812167bcad15d557cd5b83c40c70f9cf
keithyin <yinpenghhz@hotmail.com>
2021-07-24 15:02:28 +0800

    'ssr'

--- src/main.rs
+++ src/main.rs
@@ -76,6 +76,9 @@ fn read_from_local_v2() {
 //     Ok(())
 // }
 
+/// https://jiajunhuang.com/articles/2019_06_06-socks5.md.html.
+/// 文中的 client 可以理解就是 浏览器
+/// 文中的 server 对于ssr，包括两部分，一个是 本地的 server + 远程的 server。
 pub fn main() {
     remote::remote_server();
 }
